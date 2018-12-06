a1fee4e2f285fca48836849f6eb4b25efd2a629d
Sarthak Tandon <sarthak0120@gmail.com>
2018-12-06 15:41:01 +0530

    Changed distribution params

--- ALOHA.py
+++ ALOHA.py
@@ -2,48 +2,38 @@ import numpy as np
 
 # Initialize Parameters
 serviced = []
+curr_process = 0
 IAT = []
-ST = 12
 AT = []
-
+transmitting = FALSE
 
 # Input Parameters
-total_time = int(input("Enter Total Time: "))
-IAT_rate = int(input("Enter IAT Rate: "))
+
+TT = int(input("Enter Total Time(s): "))
+FT = input("Enter single frame transmission time(s): ")
+G = input("Enter Attempt Rate(per frame time): ")
 
 
-num_processes = np.random.poisson(IAT_rate)
-num_processes_served = 0
+num_transmissions = 0
+num_succ = 0
+for i in range(int(TT/FT)):
+    num_transmissions += int(np.random.poisson(G))
+    
+num_transmissions+= int(np.random.poisson(G*(TT%FT)/FT))
 
 # Populate Inter-Arrival-Times (IAT)
-for i in range(num_processes):
-    temp = int(np.random.exponential(IAT_rate))
-
-    if i==0:
-        IAT.append(0)
-    else:
-        IAT.append(temp)
+for i in range(num_transmissions-1):
+    IAT.append(int(np.random.exponential(1/G)*100))
 
 
-# Get Arrival-Times (AT) from IAT starting at t=0
-# and initialize Waiting-Times to 0
-for i in range(num_processes):
+# Get Arrival-Times (AT)
+for i in range(num_transmissions):
     if i == 0:
         AT.append(0)    
     else:
         AT.append(AT[i-1] + IAT[i])
 
-# Simulation of M/M/1 Queue (i represents current time)
-
-end_time = -1
-
-for i in range(total_time):    
-    for j in range(num_processes):
-        if i == j and i>end_time:
-            serviced.append(i)
-            num_processes_served = num_processes_served + 1
-            end_time = i + ST
-            break
+# Simulation of Pure ALOHA Protocol (i represents current time)
+for i in range(TT*100):    
     
-print ("Throughput is: ", num_processes_served/total_time)
-                
+print("Throughput is: ", num_succ/TT)
